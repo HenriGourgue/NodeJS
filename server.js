61b96276ec00bcc9f6b38e4a458807efea0893eb
Henri Gourgue <gourguehenri@gmail.com>
2020-03-16 12:50:02 +0100

    First POST

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded());
+app.use(express.json());
 const PORT = process.env.PORT || 3000;
-
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-  })
 
 app.listen(PORT, function () {
   console.log('Example app listening on port 3000!')
+})
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
 })
 
 app.get('/hello', function(req, res){
@@ -18,3 +20,20 @@ app.get('/hello', function(req, res){
         res.send('Hello ' + nom + '!\n')
     }
 })
+
+// Access the parse results as request.body
+app.post('/chat', function(req, res){
+    if(req.body.msg != undefined){
+        switch(req.body.msg){
+            case 'ville':
+                res.send('Nous sommes à Paris.\n')
+                break
+            case 'meteo':
+                res.send('Il fait beau.\n')
+                break
+            default:
+                break
+        }
+    }
+    console.log(req.body.msg);
+});
