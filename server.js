df0fd572c9e7ec688a59dfa2270181693d829e2e
Henri Gourgue <gourguehenri@gmail.com>
2020-03-16 12:10:46 +0100

    test port

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const PORT = process.env.PORT || 3000;
+
 var http = require('http');
 
 http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.end('Hello World\n');
-}).listen(3000);
+}).listen(PORT);
 
 console.log('Server started');
