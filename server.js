7c54bb17e0ede6b30fceee7dc34941795cb2548d
Henri Gourgue <gourguehenri@gmail.com>
2020-03-16 15:28:58 +0100

    Exercice 5

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(express.urlencoded());
+const fs = require('fs')
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
@@ -21,7 +21,6 @@ app.get('/hello', function(req, res){
     }
 })
 
-// Access the parse results as request.body
 app.post('/chat', function(req, res){
     if(req.body.msg != undefined){
         switch(req.body.msg){
@@ -37,3 +36,35 @@ app.post('/chat', function(req, res){
     }
     console.log(req.body.msg);
 });
+
+app.post('/chats', function(req, res){
+    if(req.body.msg != undefined){
+        var msg = req.body.msg
+        var index = msg.indexOf('=')
+        if(msg.indexOf('=')!=-1){
+            let key = msg.substring(0, index)
+            let value = msg.substring(index+1, msg.length)
+            let contenu = {
+                demain: value
+            }
+            fs.readFile("./reponses.json", (err, data) => {
+                if(data.length==0){
+                    fs.writeFile("./reponses.json",JSON.stringify(contenu), error => {
+                    if(error){
+                        console.error(error);
+                    }
+                    res.send("Merci pour cette information.")
+                    })
+                } else {
+                    if (err) throw err;
+                    let demain = JSON.parse(data);
+                    if(demain.demain!=undefined){
+                        res.send("demain: " + demain.demain)
+                    }
+                }
+            });
+        } else {
+            res.send('Je ne connais pas ' + msg + '.')
+        }
+    }
+});  
